b6753fba509117bb9bf910b256b1b7be551a80ef
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-01 17:29:08 +0530

    Style - Formatted The Code And Added Semicolons

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,33 +7,32 @@ const { body, validationResult } = require('express-validator');
 router.post('/', [
     body("name", "Enter A Valid Name").isLength({ min: 3 }),
     body("email", "Enter A Valid Email").isEmail(),
-    body("password", "Password Must Be Atleast 5 Characters").isLength({ min: 5 })
-], async (req, res) => {
+    body("password", "Password Must Be Atleast 5 Characters").isLength({ min: 5 })], async (req, res) => {
 
-    try {
-        // If There Are Errors Return Bed Request And The Errors
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
+        try {
+            // If There Are Errors Return Bed Request And The Errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email.toLowerCase(),
+                password: req.body.password
+            });
+
+            // Check Whether The User With The Same Email Exists Already
+            let user = await User.findOne({ email: req.body.email.toLowerCase() });
+            if (user) {
+                return res.status(400).json({ error: "User Alerady Exists With This Email" });
+            }
+            user = await newUser.save();
+            res.json(user);
         }
-
-        const newUser = new User({
-            name: req.body.name,
-            email: req.body.email.toLowerCase(),
-            password: req.body.password
-        });
-
-        // Check Whether The User With The Same Email Exists Already
-        let user = await User.findOne({ email: req.body.email.toLowerCase() });
-        if (user) {
-            return res.status(400).json({ error: "User Alerady Exists With This Email" });
+        catch (err) {
+            console.error(err.message);
         }
-        user = await newUser.save();
-        res.json(user)
-    }
-    catch (err) {
-        console.log(err)
-    }
-});
+    });
 
 module.exports = router;
