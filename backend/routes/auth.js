3d633ddce22d05bad194de09759ea3ac1858c55c
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-01 17:26:04 +0530

    Unique Email Validation Added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,28 +3,37 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
-// Create A User Using: POST "api/auth/". Doesn't Require Authentication
+// Create A User Using: POST "api/auth/". Login Isn't Required
 router.post('/', [
     body("name", "Enter A Valid Name").isLength({ min: 3 }),
     body("email", "Enter A Valid Email").isEmail(),
     body("password", "Password Must Be Atleast 5 Characters").isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+    try {
+        // If There Are Errors Return Bed Request And The Errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email.toLowerCase(),
+            password: req.body.password
+        });
+
+        // Check Whether The User With The Same Email Exists Already
+        let user = await User.findOne({ email: req.body.email.toLowerCase() });
+        if (user) {
+            return res.status(400).json({ error: "User Alerady Exists With This Email" });
+        }
+        user = await newUser.save();
+        res.json(user)
     }
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    newUser.save()
-        .then(user => res.json(user))
-        .catch(err => {
-            console.log(err);
-            res.json({ error: "Email Id Already Exists... Please Provide Unique Email Id...", message: err.message })
-        })
-})
+    catch (err) {
+        console.log(err)
+    }
+});
 
-module.exports = router
+module.exports = router;
