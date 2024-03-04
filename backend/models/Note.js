3b624373d45027d3b0625e27029363357d271a3e
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-04 13:00:08 +0530

    Added User Field

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const noteSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true
@@ -19,4 +24,5 @@ const noteSchema = new Schema({
     },
 });
 
-module.exports = mongoose.model("note", noteSchema);
+const Note = mongoose.model("note", noteSchema);
+module.exports = Note;
