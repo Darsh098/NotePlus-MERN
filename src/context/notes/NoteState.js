b88150a2f5dc74358c8a3c4bd8a3d31538b27fb1
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-04 23:32:16 +0530

    Added addNote, editNote, deleteNote Methods

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -23,8 +23,36 @@ const NoteState = (props) => {
         }
     ]
     const [notes, setNotes] = useState(notesIntial)
+
+    // Add A Note
+    const addNote = (title, description, tag) => {
+        const note = {
+            "_id": "65e5b9dbfa10d4f400d20a7b2",
+            "user": "65e1d54cf4647300c64677ea",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "Date": "2024-03-04T12:08:31.979Z",
+            "__v": 0
+        };
+        setNotes(notes.concat(note));
+    }
+
+    // Edit A Note
+    const editNote = (id, title, description, tag) => {
+
+    }
+
+    // Delete A Note
+    const deleteNote = (id) => {
+        console.log("Deleting The Node With Id: " + id);
+        const newNotes = notes.filter((note) => { return note._id !== id });
+        setNotes(newNotes);
+    }
+
+
     return (
-        <NoteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, addNote, editNote, deleteNote }}>
             {props.children};
         </NoteContext.Provider>
     )
