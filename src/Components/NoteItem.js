60d89619b40175b0ca38569e04be3e5153b5f0d1
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-04 23:30:37 +0530

    Used Context API To Delete The Notes

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
 
 const NoteItem = (props) => {
     const { note } = props;
+    const context = useContext(noteContext)
+    const { editNote, deleteNote } = context;
     return (
         <div className="col-md-3">
             <div className="card my-3">
@@ -9,7 +12,7 @@ const NoteItem = (props) => {
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
                     <i className="far fa-edit mx-2" />
-                    <i className="far fa-trash-alt mx-2" />
+                    <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(note._id) }} />
                 </div>
             </div>
         </div>
