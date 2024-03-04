c5f439748eeef0ea218bb9fe3e0cb2d6d090d062
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-04 16:12:18 +0530

    Setting React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import { Home } from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This Is Note+ WebApp</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router >
+    </>
   );
 }
 
