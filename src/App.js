468bbb855ae4a92c901c0c44bc8cba719aaa0f6d
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-04 17:22:40 +0530

    Used NoteState Context API

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import {
 import Navbar from './Components/Navbar';
 import { Home } from './Components/Home';
 import About from './Components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-        </Routes>
-      </Router >
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+          </Routes>
+        </Router >
+      </NoteState>
     </>
   );
 }
