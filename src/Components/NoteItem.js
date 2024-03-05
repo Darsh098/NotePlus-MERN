b9a0e29c7e1132e0cb2fc6e4e7617b006f074f16
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-05 19:00:32 +0530

    Added updateNote Method on button onclick

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -2,16 +2,16 @@ import React, { useContext } from 'react'
 import noteContext from '../context/notes/noteContext';
 
 const NoteItem = (props) => {
-    const { note } = props;
     const context = useContext(noteContext)
-    const { editNote, deleteNote } = context;
+    const { deleteNote } = context;
+    const { note, updateNote } = props;
     return (
         <div className="col-md-3">
             <div className="card my-3">
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
-                    <i className="far fa-edit mx-2" />
+                    <i className="far fa-edit mx-2" onClick={() => updateNote(note)} />
                     <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(note._id) }} />
                 </div>
             </div>
