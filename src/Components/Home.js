492445a55a0c47a2177006a54ac79bf5bcda2f58
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-06 15:42:58 +0530

    Receiving And Passing Alert As Props To The Notes Component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Notes from './Notes';
 
-export const Home = () => {
+export const Home = (props) => {
     return (
         <div className='container my-3'>
-            <Notes />
+            <Notes showAlert={props.showAlert} />
         </div>
     )
 }
