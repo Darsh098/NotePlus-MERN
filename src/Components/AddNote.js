8719a6d0288cf0522584be81b1d8a0b912e26e13
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-06 12:06:26 +0530

    Clearing TextBox After Adding A Note

--- src/Components/AddNote.js
+++ src/Components/AddNote.js
@@ -10,6 +10,7 @@ const AddNote = () => {
     const handleClick = (e) => {
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
+        setNote({ title: "", description: "", tag: "" });
     }
 
     const onChange = (e) => {
@@ -22,15 +23,15 @@ const AddNote = () => {
             <form>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
-                    <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} />
+                    <input type="text" className="form-control" value={note.title} id="title" name="title" aria-describedby="emailHelp" onChange={onChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
-                    <input type="text" className="form-control" id="description" name="description" onChange={onChange} />
+                    <input type="text" className="form-control" value={note.description} id="description" name="description" onChange={onChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" className="form-control" id="tag" name="tag" onChange={onChange} />
+                    <input type="text" className="form-control" value={note.tag} id="tag" name="tag" onChange={onChange} />
                 </div>
                 <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
             </form>
